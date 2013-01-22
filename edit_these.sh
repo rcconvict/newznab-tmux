55e1b333a37ddd064e8b56d9fb3ae09491e22ec9
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-22 21:57:32 +0100

    forgot sleep timer

--- edit_these.sh
+++ edit_these.sh
@@ -23,16 +23,16 @@ export TMUX_SESSION="Newznab-dev"
 export MONITOR_UPDATE="20"
 
 #Set, in seconds - how long the update_binaries should sleep between runs, 0 may cause errors
-export NNTP_SLEEP="20"
+export NNTP_SLEEP="40"
 
 #Set, in seconds - how long the backfill should sleep between runs, 0 may cause errors
-export BACKFILL_SLEEP="20"
+export BACKFILL_SLEEP="40"
 
 #Set, in seconds - how long the update_release should sleep between runs, 0 may cause errors
-export RELEASES_SLEEP="20"
+export RELEASES_SLEEP="40"
 
 #Set, in seconds - how long the nzb-import should sleep between runs, 0 may cause errors
-export IMPORT_SLEEP="20"
+export IMPORT_SLEEP="40"
 
 #Set the maximum days to backfill, you set the nn+ admin/edit backfill to 1
 #this will increment your database by 1 after each backfill loop
@@ -42,7 +42,7 @@ export MAXDAYS="210"
 
 #Set the path to the nzb dump you downloaded from torrents, theis is the path to bulk files folder of nzbs
 #this does not recurse through subfolders
-export NZBS="/path/to/nzbs"
+export NZBS="/home/jonnyboy/nzbs/batch"
 
 #Choose to run the threaded or non-threaded newznab scripts true/false
 #such as update_binaries.php or update_binaries_threaded.php
@@ -50,7 +50,7 @@ export THREADS="true"
 
 #Choose your database engine, comment the one true/false
 #you should have already converted your database to InnoDB engine, if you select true here
-export INNODB="true"
+export INNODB="false"
 
 #Choose to run update_cleanup.php true/false
 #set to false by default, you will need to edit /misc/testing/update_cleanup.php and /misc/testing/update_parsing.php
@@ -97,12 +97,12 @@ export USE_VNSTAT="false"
 
 #Each pane may have periods of inactivity, at the time "Pane iis Dead" will be displayed.
 #To disable my notes about why this is ok, change to false
-export SHOW_WHY="true"
+export SHOW_WHY="false"
 
 #By using this script you understand that the programmer is not responsible for any loss of data, users, or sanity.
 #You also agree that you were smart enough to make a backup of your database and files. Do you agree? yes/no
 
-export AGREED="no"
+export AGREED="yes"
 
 ##END OF EDITS##
 
